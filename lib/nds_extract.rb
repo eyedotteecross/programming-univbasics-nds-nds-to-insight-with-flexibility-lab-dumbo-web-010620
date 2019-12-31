9d44f5a33dec1185a1b084a35a103379439d5dd4
Anthony Holder <anthonyh202x@gmail.com>
2019-12-31 21:53:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,13 +49,7 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   
-  x = 0 
-  result = []
-  while x < movies_collection.size do
-  movie_data = movies_collection[x]
-  result << movie_with_director_name(name,movie_data)
-  x += 1
-  end 
+  
   
 end
 
